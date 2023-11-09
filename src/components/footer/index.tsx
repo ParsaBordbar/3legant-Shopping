ee61e29663a2542233b1d7a5defb9861e919f0b6
Mohammad Mosavat <42.octmst@gmail.com>
2023-11-09 12:49:47 +0330

    added footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,14 +1,57 @@
 const Footer = () => {
   return (
     <footer className="py-20 bg-[var(--neutral-07)] flex flex-col gap-12 items-center px-40">
-      <div>
-        <section className="flex gap-8 items-center">
+      <div className="flex w-full items-center">
+        <div className="flex-grow flex items-center gap-8">
           <picture>
-            <img src="" alt="" />
+            <img src="/headers/Logo-2.svg" alt="" />
           </picture>
-          <p className="pl-8 border-l-2 text-[var(--neutral-03)]  border-zinc-200">Headphone Store</p>
-        </section>
+          <p className="pl-8 border-l text-[var(--neutral-03)] third-font text-sm leading-5 border-[var(--neutral-04)]">
+            Headphone Store
+          </p>
+        </div>
+        <nav className="flex items-center gap-10">
+          <li className="list-none third-font text-sm leading-5 text-[var(--neutral-01)]">
+            Home
+          </li>
+          <li className="list-none third-font text-sm leading-5 text-[var(--neutral-01)]">
+            Shop
+          </li>
+          <li className="list-none third-font text-sm leading-5 text-[var(--neutral-01)]">
+            Product
+          </li>
+          <li className="list-none third-font text-sm leading-5 text-[var(--neutral-01)]">
+            Blog
+          </li>
+          <li className="list-none third-font text-sm leading-5 text-[var(--neutral-01)]">
+            Contact Us
+          </li>
+        </nav>
       </div>
+      <section className="border-t flex items-center w-full py-4">
+        <div className="flex items-center flex-grow gap-7">
+          <p className="sec-font text-xs leading-5 text-[var(--neutral-03)]">
+            Copyright © 2023 3legant. All rights reserved
+          </p>
+          <p className="sec-font leading-5 text-xs font-semibold text-[var(--neutral-01)]">
+            Privacy Policy
+          </p>
+          <p className="sec-font leading-5 text-xs font-semibold text-[var(--neutral-01)]">
+            Terms of Use
+          </p>
+        </div>
+        <section className="flex items-center gap-6">
+          <picture>
+            <img src="/social/instagram.svg" alt="" />
+          </picture>
+          <picture>
+            <img src="/social/facebook.svg" alt="" />
+          </picture>
+          <picture>
+            <img src="/social/youtube.svg" alt="" />
+          </picture>
+        </section>
+      </section>
     </footer>
   );
 };
