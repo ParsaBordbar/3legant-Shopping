307d13d7bc494c96b0e246ad68c16cd98272c5eb
MohammadMosavat <42.octmst@gmail.com>
2024-01-03 13:19:35 +0330

    some change

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client";
 import { Rating } from "@material-tailwind/react";
- 
-const DefaultRating = (props:{className?:string}) => {
+
+const DefaultRating = (props: { className?: string }) => {
   return (
-
-        <Rating className={`flex ${props.className} rete gap-0.5`} value={3} />
-
+    <>
+      <Rating className={`flex ${props.className} rete gap-0.5`} value={3} />
+    </>
   );
-}
+};
 
-export default DefaultRating
+export default DefaultRating;
