90c8e99d342013ee4c5166c50a800704dd79a218
MohammadMosavat <42.octmst@gmail.com>
2024-01-04 18:53:06 +0330

    added location in the information in contact us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,53 +1,77 @@
+import MainInput from "@/components/MainInput";
+import ContactFormComponent from "@/components/contactForm";
+
 const ContactUs = () => {
+
   return (
-    <div className="w-full flex flex-col gap-10 items-center">
-      <div className="flex flex-col items-start gap-2">
+    <div className="w-full px-40 flex flex-col gap-10 ">
+      <div className="flex flex-col self-start gap-2">
         <h1 className="font-bold text-3xl sec-font">Conatct US</h1>
         <p className="primary-font">
           Any question or remarks? Just write us a message!
         </p>
       </div>
-      <section className="bg-zinc-100 p-2.5 rounded-lg">
-        <div className="flex rounded-md p-10   bg-[var(--bg-items-pop-over)] flex-col items-start gap-4">
-          <div>
+      <section className="bg-zinc-100 w-full flex items-center p-2.5 rounded-lg">
+        <div className="flex w-1/3 rounded-md p-10 bg-[var(--bg-items-pop-over)] flex-col items-start gap-4">
+          <div className="mb-24 flex flex-col items-start gap-1.5">
             <p className="font-bold text-xl sec-font">Contact Information</p>
             <p className="primary-font">Say something to start a live chat!</p>
           </div>
-          <ul>
-            <li>
-              <picture>
-                <img src="" alt="" />
+          <ul className="flex mb-24 flex-grow flex-col gap-[3.125rem]">
+            <li className="flex items-center gap-6">
+              <picture className="w-6 h-6 inline-block">
+                <img
+                  className="w-full h-full"
+                  src="/icons/phone-call.svg"
+                  alt=""
+                />
               </picture>
               <p>+1012 3456 789</p>
             </li>
-            <li>
-              <picture>
-                <img src="" alt="" />
+            <li className="flex items-center gap-6">
+              <picture className="w-6 h-6 inline-block">
+                <img className="w-full h-full" src="/social/mail.svg" alt="" />
               </picture>
               <p>3legant@gmail.com</p>
             </li>
-            <li>
-              <picture>
-                <img src="" alt="" />
+            <li className="flex items-center gap-6">
+              <picture className="w-6 h-6 inline-block">
+                <img
+                  className="w-full h-full"
+                  src="/icons/location-filled.svg"
+                  alt=""
+                />
               </picture>
               <p>
                 132 Dartmouth Street Boston, Massachusetts 02156 United States
               </p>
             </li>
           </ul>
-          <div>
-            <picture>
-              <img src="" alt="" />
+          <div className="flex items-center gap-6">
+            <picture className="w-8 h-8 inline-block cursor-pointer">
+              <img
+                className="w-full h-full rounded-full"
+                src="/icons/x.svg"
+                alt=""
+              />
             </picture>
-            <picture>
-              <img src="" alt="" />
+            <picture className="w-8 h-8 inline-block cursor-pointer">
+              <img
+                className="w-full h-full rounded-full"
+                src="/icons/instagram.svg"
+                alt=""
+              />
             </picture>
-            <picture>
-              <img src="" alt="" />
+            <picture className="w-8 h-8 inline-block cursor-pointer">
+              <img
+                className="w-full h-full rounded-full"
+                src="/icons/facebook.svg"
+                alt=""
+              />
             </picture>
           </div>
         </div>
-        <section></section>
+        <ContactFormComponent/>
       </section>
     </div>
   );
