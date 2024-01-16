16d029e13128f359e57083e98049e1b8ff907a82
MohammadMosavat <42.octmst@gmail.com>
2024-01-16 21:20:00 +0330

    added icons by picture tag to items

--- src/components/PopOverItems/index.tsx
+++ src/components/PopOverItems/index.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 
-const PopOverItems = (props: { value: string; link?: string }) => {
+const PopOverItems = (props: {
+  value: string;
+  link?: string;
+  img?: string;
+}) => {
   return (
     <Link
-      href={props.link ?? ''}
-      className="p-1 px-2 outline-none hover:py-3 sec-font cursor-pointer hover:bg-[var(--bg-pop-over)] transition-all ease-in-out duration-300 rounded-md"
+      href={props.link ?? ""}
+      className="p-1 px-2 flex items-center gap-1 outline-none hover:py-3 sec-font cursor-pointer hover:bg-[var(--bg-pop-over)] transition-all ease-in-out duration-300 rounded-md"
     >
-      {props.value}
+      {props.img && (
+        <picture className="w-6 h-6">
+          <img className="w-full h-full" src={props.img} alt="" />
+        </picture>
+      )}
+      <p className="">{props.value}</p>
     </Link>
   );
 };
