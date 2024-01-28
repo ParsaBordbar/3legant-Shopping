c45b1a7ea96c1ef0bd94a360573b1910bb68e9d0
MohammadMosavat <42.octmst@gmail.com>
2024-01-28 19:41:48 +0330

    add page for all product

---
+++ src/app/product/main/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import CartProduct from "@/components/cardProducts";
+import { DataProduct } from "@/data";
+import { useCallback } from "react";
+
+const MainRoute = () => {
+  const GetAllData = useCallback(() => {
+    return DataProduct.map((items) => {
+      return (
+        <CartProduct
+          id={items.id}
+          img={items.img}
+          name={items.name}
+          cato={items.cato}
+          price={items.price}
+          pos={items.pos}
+          key={items.id}
+          information={items.information}
+          useFull={items.useFull}
+        />
+      );
+    });
+  }, []);
+  return GetAllData();
+};
+
+export default MainRoute;
