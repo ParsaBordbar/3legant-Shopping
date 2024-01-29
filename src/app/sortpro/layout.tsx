b64f411e8ea05ea244b767d6664383bec89ca175
MohammadMosavat <42.octmst@gmail.com>
2024-01-30 00:48:39 +0330

    import component of categories of product and call children in main tag

--- src/app/sortpro/layout.tsx
+++ src/app/sortpro/layout.tsx
@@ -1,12 +1,14 @@
+import CategoriesProduct from "@/components/CateProducts";
 import Newsletter from "@/components/Newsletter";
 import { ReactNode } from "react";
 
 const LayoutSortProducts = ({ children }: { children: ReactNode }) => {
   return (
     <div className=" w-[84%] mx-auto mt-10">
-      {children}
+      <CategoriesProduct />
+      <main>{children}</main>
     </div>
   );
 };
 
-export default LayoutSortProducts
+export default LayoutSortProducts;
