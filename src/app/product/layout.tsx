23beb9681ad7943bc4a0fe0c38aba45f60098cfd
MohammadMosavat <42.octmst@gmail.com>
2024-01-30 20:05:06 +0330

    import a component for chose categories

--- src/app/product/layout.tsx
+++ src/app/product/layout.tsx
@@ -1,30 +1,14 @@
+
+import CategoriesProduct from "@/components/CateProducts";
 import CollecItem from "@/components/collecItems";
 import { ReactNode } from "react";
 
 const LayoutProductPage = ({ children }: { children: ReactNode }) => {
+
   return (
     <div className="w-[85%] mx-auto mt-10">
-      <ul className="w-full h-fit gap-2 grid grid-rows-2 grid-cols-2">
-        <CollecItem
-          className="row-span-2 !h-[454px]"
-          img={"/collec/headphone.svg"}
-          title={"Headphones"}
-          link={""}
-        />
-        <CollecItem
-          className=""
-          img={"/collec/earbuds.svg"}
-          title={"Earbuds"}
-          link={""}
-        />
-        <CollecItem
-          className=""
-          img={"/collec/accessories.svg"}
-          title={"Accessories"}
-          link={""}
-        />
-      </ul>
-      <main className="flex mt-10 justify-between items-center gap-6 flex-wrap">{children}</main>
+      <CategoriesProduct/>
+      <main className="flex  justify-between items-center gap-6 flex-wrap">{children}</main>
     </div>
   );
 };
