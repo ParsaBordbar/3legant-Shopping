29736961d91e95a89ea55d8b7469803f82c8bc55
MohammadMosavat <42.octmst@gmail.com>
2024-01-30 20:02:26 +0330

    add component for items of user account

---
+++ src/components/accountItems/index.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const AccountItems = (props: { link: string; title: string; SvgComponent: any }) => {
+  return (
+      <Link href={props.link} className="flex w-full hover:bg-[--bg-pop-over] rounded-md transition-all ease-in-out duration-200 p-1.5 items-center gap-2">
+        <picture className="w-6 h-6 inline-block">
+          {props.SvgComponent}
+        </picture>
+        <p className="sec-font text-base">{props.title}</p>
+      </Link>
+  );
+};
+
+export default AccountItems;
