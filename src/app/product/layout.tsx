a48d73985fc7f646d6a0d1a8b0d27ed2d781d481
MohammadMosavat <42.octmst@gmail.com>
2024-02-02 20:03:11 +0330

    add a component of categoties of products

--- src/app/product/layout.tsx
+++ src/app/product/layout.tsx
@@ -1,14 +1,14 @@
-
 import CategoriesProduct from "@/components/CateProducts";
 import CollecItem from "@/components/collecItems";
 import { ReactNode } from "react";
 
 const LayoutProductPage = ({ children }: { children: ReactNode }) => {
-
   return (
     <div className="w-[85%] mx-auto mt-10">
-      <CategoriesProduct/>
-      <main className="flex  justify-between items-center gap-6 flex-wrap">{children}</main>
+      <CategoriesProduct />
+      <main className="flex min-[400px]:justify-center sm:justify-between items-center gap-6 flex-wrap">
+        {children}
+      </main>
     </div>
   );
 };
