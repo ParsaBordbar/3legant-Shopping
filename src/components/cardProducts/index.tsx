23c40e1f58142c8158cb977824f68c2e527f5030
MohammadMosavat <42.octmst@gmail.com>
2024-05-10 21:58:38 +0330

    remove the use call backs in card profuct

--- src/components/cardProducts/index.tsx
+++ src/components/cardProducts/index.tsx
@@ -6,58 +6,28 @@ import { TProduct } from "@/types";
 import Link from "next/link";
 
 const CartProduct = (props: TProduct) => {
-  const [show, setShow] = useState<boolean>(false);
-
-  const ButtonProduct = useMemo(() => {
-    return (
-      show && (
-        <Link href={`/sinpro/${props.id}`}>
-          <MainButton
-            className="w-[230px]  absolute bottom-4 left-4 py-2 px-6"
-            value={"Add To Cart"}
-          />
-        </Link>
-      )
-    );
-  }, [show]);
-
-  const HeartProduct = useMemo(() => {
-    return (
-      show && (
-        <picture className="inline-block drop-shadow top-4 left-4 cursor-pointer rounded-full p-1.5 bg-white shadow-[var(--box-shadow)]">
-          <img
-            className="overflow-hidden"
-            alt={props.name}
-            src="/productsIcon/heart.svg"
-          />
-        </picture>
-      )
-    );
-  }, [show]);
-
   return (
-    <div
-      // onMouseEnter={() => setShow(true)}
-      // onMouseLeave={() => setShow(false)}
-      className="flex gap-3 group w-[262px] flex-col"
-    >
+    <div className="flex gap-3 group w-[262px] flex-col">
       <section className="h-[349px] relative">
         <div className="absolute top-4 left-4 right-4 flex items-center justify-between">
           <p className=" uppercase cursor-default px-[14px] font-bold py-1 rounded bg-white text-base third-font">
             {props.pos}
           </p>
           <picture className="group-hover:inline-block hidden drop-shadow top-4 left-4 cursor-pointer rounded-full p-1.5 bg-white shadow-[var(--box-shadow)]">
-          <img
-            className="overflow-hidden"
-            alt={props.name}
-            src="/productsIcon/heart.svg"
-          />
-        </picture>
+            <img
+              className="overflow-hidden"
+              alt={props.name}
+              src="/productsIcon/heart.svg"
+            />
+          </picture>
         </div>
         <picture className="w-[262px] h-full">
           <img className="w-full h-full" src={props.img} alt="" />
         </picture>
-        <Link className="group-hover:inline-block hidden" href={`/sinpro/${props.id}`}>
+        <Link
+          className="group-hover:inline-block hidden"
+          href={`/sinpro/${props.id}`}
+        >
           <MainButton
             className="w-[230px]  absolute bottom-4 left-4 py-2 px-6"
             value={"Add To Cart"}
