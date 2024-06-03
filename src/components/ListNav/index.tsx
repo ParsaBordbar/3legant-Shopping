38baf4e62598909cca7fbdae977f37a7f26c7c24
MohammadMosavat <42.octmst@gmail.com>
2024-06-03 16:36:43 +0330

    change the font size of list nav component

--- src/components/ListNav/index.tsx
+++ src/components/ListNav/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const ListNav = (props: { value: string , link?:string }) => {
   return (
     <Link href={props.link ?? ''}>
-      <li className="text-black primary-font font-semibold list-none text-sm">
+      <li className="text-black primary-font font-semibold list-none text-base">
         {props.value}
       </li>
     </Link>
